70a3a3f4e917d74f28262037d73b1070a0a9f4f5
tarokov <publictaro@gmail.com>
2021-04-29 12:56:57 +0900

    dynamodb error removed: error updating DynamoDB Table (Tweets) time to live: error updating DynamoDB Table (Tweets) Time To Live: ValidationException: TimeToLive is already disabled

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -16,11 +16,6 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "S"
   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
   tags = {
     Name        = "dynamodb-table-1"
     Environment = "production"
